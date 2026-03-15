1adb33598980f17649814840cba4b5901c5782a2
thegostisdead <thegostisdead@gmail.com>
2026-03-15 02:31:52 +0100

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://pastesong.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://pastesong.vercel.app/llms.txt",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
+}
